e1318280b0665d7806c547d68cad7c832e093617
Devin Cooper <devindcooper@gmail.com>
2025-10-25 22:14:53 -0700

    Arguments

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import sys
+
 from stats import get_num_words, get_char_count, sort_on,get_letter_count
 
+book_path = sys.argv
+if len(book_path) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+
+    sys.exit(1)
+else:
+    with open(book_path[1]) as f:
+        file_content = f.read()
 
 
-with open("books/frankenstein.txt") as f:
-    file_content = f.read()
 
 def get_book_text(file_path):
     print(file_path)
@@ -16,7 +24,7 @@ char_counts.sort(reverse = True, key=sort_on)
 
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {book_path[1]}.")
 print("----------- Word Count ----------")
 word_count = get_num_words(file_content)
 print("--------- Character Count -------")
